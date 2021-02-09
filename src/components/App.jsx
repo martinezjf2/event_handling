617c735ef2645622ad8717f19038577546426098
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-02-08 23:36:56 -0500

    using useState, and implementing the onMouseOver and the onMouseOut events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,35 @@
-import React from "react";
-
-function handleClick() {
-  console.log("Clicked");
-}
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMousedOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseOver() {
+    // console.log("hovered in");
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    // console.log("hovered out");
+    setMouseOver(false);
+  }
+
   return (
     <div className="container">
-      <h1>{heading}</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button onClick={handleClick}>Submit</button>
+      <button
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
